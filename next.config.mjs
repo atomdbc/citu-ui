92bd8650c079175d0c99721a732412032b346e61
samuel lemikan <nexigen0@gmail.com>
2024-12-14 19:14:48 +0100

    addedd more sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ];
+  },
 }
 
 export default nextConfig;
